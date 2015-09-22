6f00620003c9c911b4de2442ccb58b111c154c67
serea77 <sebastien.reader.1@ulaval.ca>
2015-09-22 15:33:22 -0400

    modification de la bare de menu

--- script/menu.js
+++ script/menu.js
@@ -16,3 +16,32 @@ function ToggleMenu() {
         mainContainerElement.style.left = 0;
     }
 }
+
+function windowConnection(){
+   // alert("bonjour les tous petit");
+
+    window.open("connection.html","Connection", 'height=150,width=200,top=110,left=1600,resizable=no' );
+}
+
+function passwordValidation()
+{
+    var userName = document.getElementById("userInput");
+    var password = document.getElementById("passWordInput");
+    if(userName.value==="serea" && password.value==="12345"){
+
+        alert("Bienvenue Sebastien");
+        window.close();
+        document.getElementById("userMenu").style.opacity=100;
+        document.getElementById("connection").style.opacity=0;
+        console.log(document.getElementById("userMenu").style.opacity);
+        
+
+
+    }
+    else{
+        alert("This user not exist or the password is false.")
+    }
+
+
+
+}
