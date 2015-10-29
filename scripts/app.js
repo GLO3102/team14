7fe9c8dadc90eee3c5ab72dede8c72196f5ae060
serea77 <sebastien.reader.1@ulaval.ca>
2015-10-29 12:05:53 -0400

    ajout de la page de film dans le fichier filmTemplate.html

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Sebastien on 2015-10-26.
+ */
+
+var movieVue = new MovieView();
+movieVue.render();
--- scripts/views/movies.js
+++ scripts/views/movies.js
@@ -1,3 +1,47 @@
 /**
- * Created by Sebastien on 2015-10-19.
+ * Created by Sebastien on 2015-10-26.
  */
+MovieView = Backbone.View.extend({
+    el: "#movies-lists",
+    render: function(){
+        var that = this;
+        var formData = {email:"sebastien.reader.1@ulaval.ca", password:"serea@ulaval@2013"};
+        $.ajax({
+            url : "https://umovie.herokuapp.com/login",
+            type: "POST",
+            data : formData,
+            success: function(data, textStatus, jqXHR)
+            {
+                var securityToken = data.token;
+                var v_trackId = 380239015;
+
+                var movie = new MovieModel({trackId: v_trackId});
+                movie.urlRoot += "/" +v_trackId;
+                movie.fetch({headers: {
+                    'Authorization': securityToken} ,
+                    success: function (data) {
+                        var film = data.toJSON();
+                        var result = film.results[0];
+                        result =  changeFilmStatsFormat(result);
+                        var template = _.template($("#movie-template").html());
+                        that.$el.html(template({movie: result}));
+                    }
+                })
+
+
+            }});
+
+    }
+
+
+
+},
+changeFilmStatsFormat = function(filmArray){
+    var date =  filmArray.releaseDate;
+    filmArray.releaseDate = date.slice(0,10);
+    var filmTime = filmArray.trackTimeMillis;
+    var millisInMinute = 60000;
+    filmTime = (filmTime /millisInMinute);
+    filmArray.trackTimeMillis = Math.round(filmTime);
+    return filmArray;
+})
