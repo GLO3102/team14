faaba7c7bc03b8673804592d44928f3ab2ad6e97
pascal129 <pascal.labb129@gmail.com>
2015-11-01 10:55:36 -0500

    Ajustement du style

--- scripts/collection/episodes.js
+++ scripts/collection/episodes.js
@@ -2,14 +2,19 @@ $(function (){
     EpisodesCollection =  Backbone.Collection.extend({
         model: EpisodesModel,
         getCollectionId: function (response) {
-            console.log(response);
-
             return 0;
         },
         parse: function(response) {
             var filterBySeason = response.results.filter(function (el) {
                 return el.collectionId == 533936970;
             });
+            filterBySeason.forEach(function(obj) {
+                var trackName = obj.trackName;
+                trackName = trackName.substring(trackName.indexOf("\"")+1);
+                trackName = trackName.substring(0,trackName.indexOf("\""));
+                obj.trackName = trackName;
+            });
+
             return filterBySeason;
         }
     });
--- scripts/collection/tvshows.js
+++ scripts/collection/tvshows.js
@@ -2,8 +2,17 @@ $(function (){
     TvShowsCollection =  Backbone.Collection.extend({
         model: TVShowsModel,
         parse: function(response) {
+            response = response.results;
 
-            return response.results;
+            var monthNames = ["January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December"
+            ];
+            response.forEach(function(obj) {
+                var releaseDate = new Date(obj.releaseDate);
+                releaseDate = monthNames[releaseDate.getMonth()] + " "+releaseDate.getUTCDate() + ", "+releaseDate.getUTCFullYear() ;
+                obj.releaseDate = releaseDate;
+            });
+            return response;
         }
     });
 });
