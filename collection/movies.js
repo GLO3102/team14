2e7a99ee8b7dc77cafd384850551f205d823d248
stephane1 <stephane-alain.kengne-fogang.1@ulaval.ca>
2015-11-04 21:02:31 -0500

    mecredi

---
+++ collection/movies.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Sebastien on 2015-10-19.
+ */
+$(function() {
+ MoviesCollection = Backbone.Collection.extend({
+    model:MovieModel,
+
+      });
+});
--- scripts/models/actor.js
+++ scripts/models/actor.js
@@ -4,14 +4,8 @@
 
 (function() {
     ActorModel = Backbone.Model.extend({
-        defaults: {
-            actor: ''
-        },
-        parse: function(response) {
-            // Make sure the id is valid.
-            this.id = response.id;
-            return response;
-        }
+
+
 
     });
 })();
