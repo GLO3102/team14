354aac9152a1be79c04579ec9f9a05f86a230ee9
mopigeons <simon@mopigeons.com>
2015-12-03 15:24:04 -0500

    Système de recommendations fonctionnel

--- scripts/recommendations.js
+++ scripts/recommendations.js
@@ -4,24 +4,44 @@
 
 var uMovieRecTotal = 0;
 
-var recCallback = function(data) {
+var recCallback = function(data, type) {
     var recList = [];
     for(var i = 0; i<data.Similar.Results.length; i++) {
         var itemName = data.Similar.Results[i].Name;
         var itemType = data.Similar.Results[i].Type;
         recList.push({"name": itemName, "type": itemType});
     }
-    var uMovieRecList = createUMovieRecList(recList);
+    var uMovieRecList = createUMovieRecList(recList, type);
+}
 
-
+var recCallbackMovies = function(data) {
+    recCallback(data, "movies");
 };
+
+var recCallbackShows = function(data) {
+    recCallback(data, "shows");
+};
+
+var recCallbackActor = function(data) {
+    recCallback(data, "actor");
+};
+
+
 
 var getRecommendationList = function(title, type) {
     uMovieRecTotal=0;
     var titleToSearch = prepareTitle(title);
     console.log("title to search is:"+titleToSearch);
+    var callbackMethod = "recCallback";
+    if(type=="shows") {
+        callbackMethod = callbackMethod + "Shows";
+    } else if (type=="movies") {
+        callbackMethod = callbackMethod + "Movies";
+    } else if (type=="actor") {
+        callbackMethod = callbackMethod + "Actor";
+    }
     var urlBegin = "https://www.tastekid.com/api/similar?q=" + encodeURIComponent(titleToSearch) + "&type=" + type;
-    var urlEnd = "&limit=5&format=JSON&jsonp=?&k=179633-uMovie-8BNUD4NB&callback=recCallback";
+    var urlEnd = "&limit=5&format=JSON&jsonp=?&k=179633-uMovie-8BNUD4NB&callback="+callbackMethod;
     var urlComplete = urlBegin + urlEnd;
 
     $.ajax({
@@ -36,16 +56,23 @@ var getRecommendationList = function(title, type) {
     })
 };
 
-var createUMovieRecList = function(recList) {
+var createUMovieRecList = function(recList, type) {
+    console.log("Simon??");
     var uMovieRecList = [];
     //for each item in recList
     var recListLength = recList.length;
     var i = 0;
     for(var i = 0; i < recListLength; i++) {
-        var subUrl = "";
-        //subUrl sera pour alterner entre "search movies" et "search shows"
+        var subUrl = "movies";
+        if(type==="movies") {
+            subUrl = "movies";
+        } else if (type==="shows") {
+            subUrl = "tvshows/seasons";
+        } else if (type==="actor") {
+            subUrl = "actors";
+        }
         console.log(recList[i].name);
-        var url = "https://umovie.herokuapp.com/unsecure/search/movies?q="+encodeURIComponent(recList[i].name)+"&limit=1";
+        var url = "https://umovie.herokuapp.com/unsecure/search/"+subUrl+"?q="+encodeURIComponent(recList[i].name)+"&limit=1";
         //var url = "https://umovie.herokuapp.com/unsecure/search/movies?q="+encodeURIComponent("attack of the killer" +
         //        " tomatoes")+"&limit=1";
         $.ajax({
@@ -53,37 +80,60 @@ var createUMovieRecList = function(recList) {
             url: url,
             contentType: 'application/json',
             success: function(data) {
-                addResponseToList(uMovieRecList, data, i, recListLength);
+                addResponseToList(uMovieRecList, data, i, recListLength, type);
             }
         });
     }
 };
 
-var addResponseToList = function(uMovieRecList, response, index, recListLength) {
+var addResponseToList = function(uMovieRecList, response, index, recListLength, type) {
     console.log("index="+index);
-    //ajoute le résultat à la liste
+    if(response === undefined) {
+        return;
+    }
+    //ajoute le résultat à la liste si pas undefined
     uMovieRecList.push(response.results[0]);
     uMovieRecTotal = uMovieRecTotal+1;
     if(uMovieRecTotal===recListLength) {
         //si c'est le dernier élément, lancer la fonction d'affichage
         console.log("here");
         console.log(uMovieRecList);
-        displayRecommendations(uMovieRecList);
+        displayRecommendations(uMovieRecList, type);
     }
 };
 
-var displayRecommendations = function(uMovieRecList) {
+var displayRecommendations = function(uMovieRecList, type) {
     console.log("display time");
     console.log(uMovieRecList.length);
+    var divtype = "movie";
+    if(type==="movies") {
+        divtype = "movie";
+    } else if (type==="shows") {
+        divtype = "show";
+    }
+    else if (type==="actor") {
+        divtype = "actor";
+    }
+    console.log("RECS TO DISPLAY"+uMovieRecList.length);
     if(uMovieRecList.length===0) {
         //todo: show the first box and write "no similar items found"
     }
     for(var i = 0; i<uMovieRecList.length; i++) {
         if(uMovieRecList[i]!==undefined) {
-            var divid = "#movieRec" + (i + 1);
-            $(divid).text(uMovieRecList[i].trackCensoredName);
-            $(divid).show();
-            $(divid).prop("href","#/movies/"+uMovieRecList[i].trackId);
+            var divid = "#"+divtype+"Rec" + (i + 1);
+            if(divtype=="movie") {
+                $(divid).text(uMovieRecList[i].trackCensoredName);
+                $(divid).prop("href","#/"+divtype+"s/"+uMovieRecList[i].trackId);
+                $(divid).show();
+            } else if (divtype == "show") {
+                $(divid).text(uMovieRecList[i].collectionName);
+                $(divid).prop("href","#/tv"+divtype+"/"+uMovieRecList[i].collectionId);
+                $(divid).show();
+            } else if (divtype == "actor") {
+                $(divid).text(uMovieRecList[i].artistName);
+                $(divid).prop("href","#/"+divtype+"s/"+uMovieRecList[i].artistId);
+                $(divid).show();
+            }
         }
     }
 
